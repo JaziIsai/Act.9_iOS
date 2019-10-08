19e4069f4df9e5f154d3906d8466808786bffab2
JaziIsai <54283557+JaziIsai@users.noreply.github.com>
2019-10-07 22:51:19 -0500

    Add files via upload

---
+++ Act9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Act9
+//
+//  Created by Ricardo López Hernández on 14/09/18.
+//  Copyright © 2018 Ricardo Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
